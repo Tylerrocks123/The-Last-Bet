56f65afd93b2e1a5255bea08da540d245fcb7899
Tylerrocks123 <taalmeida@email.neit.edu>
2025-11-05 12:19:43 -0500

    MAIN STUFF

---
+++ js/game.js
@@ -0,0 +1,26 @@
+/*-------------------------------------------
+Game Setup
+ 1. canvas 
+ 2. context
+-------------------------------------------*/
+var c = document.querySelector(`canvas`)
+var ctx = c.getContext(`2d`)
+
+
+/*------------Declare Variables Here--------*/
+//var Money = 100
+//var multiply = 1
+
+
+
+/*--------------main()------------------------
+This is the function that makes the game work
+---------------------------------------------*/
+
+function main()
+{
+}
+
+function rand{
+  //generate random number 1-10 3 times
+}
